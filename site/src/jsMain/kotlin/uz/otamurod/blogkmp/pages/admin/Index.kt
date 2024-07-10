f02ef3c87d41fc01c329954779bfd9ee4577317a
otamurod <safarovotamurod@gmail.com>
2024-07-10 16:37:07 +0500

    feat: create HomeContent

--- site/src/jsMain/kotlin/uz/otamurod/blogkmp/pages/admin/Index.kt
+++ site/src/jsMain/kotlin/uz/otamurod/blogkmp/pages/admin/Index.kt
@@ -1,8 +1,17 @@
 package uz.otamurod.blogkmp.pages.admin
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.varabyte.kobweb.compose.css.Cursor
+import com.varabyte.kobweb.compose.css.FontWeight
+import com.varabyte.kobweb.compose.css.TextAlign
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.graphics.Colors
@@ -10,27 +19,40 @@ import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
 import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
 import com.varabyte.kobweb.compose.ui.modifiers.color
 import com.varabyte.kobweb.compose.ui.modifiers.cursor
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
+import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.fontWeight
 import com.varabyte.kobweb.compose.ui.modifiers.height
 import com.varabyte.kobweb.compose.ui.modifiers.margin
 import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
 import com.varabyte.kobweb.compose.ui.modifiers.onClick
+import com.varabyte.kobweb.compose.ui.modifiers.padding
 import com.varabyte.kobweb.compose.ui.modifiers.position
 import com.varabyte.kobweb.compose.ui.modifiers.size
+import com.varabyte.kobweb.compose.ui.modifiers.textAlign
 import com.varabyte.kobweb.compose.ui.styleModifier
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.core.rememberPageContext
+import com.varabyte.kobweb.silk.components.graphics.Image
 import com.varabyte.kobweb.silk.components.icons.fa.FaPlus
 import com.varabyte.kobweb.silk.components.icons.fa.IconSize
+import com.varabyte.kobweb.silk.components.text.SpanText
 import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
 import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
 import org.jetbrains.compose.web.css.Position
+import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.css.vh
 import uz.otamurod.blogkmp.components.AdminPageLayout
+import uz.otamurod.blogkmp.models.RandomJoke
 import uz.otamurod.blogkmp.models.Theme
 import uz.otamurod.blogkmp.navigation.Screen
+import uz.otamurod.blogkmp.util.Constants.FONT_FAMILY
 import uz.otamurod.blogkmp.util.Constants.PAGE_WIDTH
+import uz.otamurod.blogkmp.util.Constants.SIDE_PANEL_WIDTH
+import uz.otamurod.blogkmp.util.Res
 import uz.otamurod.blogkmp.util.isUserLoggedIn
 
 @Page
@@ -41,8 +63,86 @@ fun HomePage() {
 
 @Composable
 fun HomeScreen() {
+    var randomJoke: RandomJoke? by remember { mutableStateOf(null) }
+
+    LaunchedEffect(Unit) {
+        randomJoke = RandomJoke(
+            id = 2,
+            joke = "Q:Why did the blog writer become a detective?:To uncover the hidden truths and captivating stories behind clickbait titles!"
+        )
+    }
+
     AdminPageLayout {
+        HomeContent(randomJoke = randomJoke)
         AddButton()
+    }
+}
+
+@Composable
+fun HomeContent(randomJoke: RandomJoke?) {
+    val breakpoint = rememberBreakpoint()
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(left = if (breakpoint > Breakpoint.MD) SIDE_PANEL_WIDTH.px else 0.px),
+        contentAlignment = Alignment.Center
+    ) {
+        if (randomJoke != null) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(topBottom = 50.px),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                if (randomJoke.id != -1) {
+                    Image(
+                        modifier = Modifier
+                            .size(150.px)
+                            .margin(bottom = 50.px),
+                        src = Res.Image.laugh,
+                        alt = "Laugh Image"
+                    )
+                }
+                if (randomJoke.joke.contains("Q:")) {
+                    SpanText(
+                        modifier = Modifier
+                            .margin(bottom = 14.px)
+                            .fillMaxWidth(40.percent)
+                            .textAlign(TextAlign.Center)
+                            .color(Theme.Secondary.rgb)
+                            .fontSize(28.px)
+                            .fontFamily(FONT_FAMILY)
+                            .fontWeight(FontWeight.Bold),
+                        text = randomJoke.joke.split(":")[1].dropLast(1)
+                    )
+                    SpanText(
+                        modifier = Modifier
+                            .fillMaxWidth(40.percent)
+                            .textAlign(TextAlign.Center)
+                            .color(Theme.HalfBlack.rgb)
+                            .fontSize(20.px)
+                            .fontFamily(FONT_FAMILY)
+                            .fontWeight(FontWeight.Normal),
+                        text = randomJoke.joke.split(":").last()
+                    )
+                } else {
+                    SpanText(
+                        modifier = Modifier
+                            .margin(bottom = 14.px)
+                            .fillMaxWidth(40.percent)
+                            .textAlign(TextAlign.Center)
+                            .color(Theme.Secondary.rgb)
+                            .fontFamily(FONT_FAMILY)
+                            .fontSize(28.px)
+                            .fontWeight(FontWeight.Bold),
+                        text = randomJoke.joke
+                    )
+                }
+            }
+        } else {
+            println("Loading content...")
+        }
     }
 }
 
