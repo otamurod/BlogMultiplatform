ada8210fa9f85b88bcb2419ff55687bc688c3a6b
otamurod <safarovotamurod@gmail.com>
2024-07-10 16:06:59 +0500

    feat: create AdminPageLayout

--- site/src/jsMain/kotlin/uz/otamurod/blogkmp/pages/admin/Create.kt
+++ site/src/jsMain/kotlin/uz/otamurod/blogkmp/pages/admin/Create.kt
@@ -1,16 +1,13 @@
 package uz.otamurod.blogkmp.pages.admin
 
 import androidx.compose.runtime.Composable
-import com.varabyte.kobweb.compose.foundation.layout.Box
-import com.varabyte.kobweb.compose.foundation.layout.Column
-import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
-import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
 import com.varabyte.kobweb.core.Page
-import org.jetbrains.compose.web.css.px
-import uz.otamurod.blogkmp.components.SidePanel
-import uz.otamurod.blogkmp.util.Constants.PAGE_WIDTH
+import org.jetbrains.compose.web.dom.Text
+import uz.otamurod.blogkmp.components.AdminPageLayout
 import uz.otamurod.blogkmp.util.isUserLoggedIn
 
 @Page
@@ -21,16 +18,12 @@ fun CreatePage() {
 
 @Composable
 fun CreateScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .maxWidth(PAGE_WIDTH.px)
+    AdminPageLayout {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
         ) {
-            SidePanel(onMenuClick = {})
+            Text(value = "Create")
         }
     }
 }
--- site/src/jsMain/kotlin/uz/otamurod/blogkmp/pages/admin/Index.kt
+++ site/src/jsMain/kotlin/uz/otamurod/blogkmp/pages/admin/Index.kt
@@ -1,21 +1,13 @@
 package uz.otamurod.blogkmp.pages.admin
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import com.varabyte.kobweb.compose.foundation.layout.Box
-import com.varabyte.kobweb.compose.foundation.layout.Column
-import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
-import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
 import com.varabyte.kobweb.core.Page
-import org.jetbrains.compose.web.css.px
-import uz.otamurod.blogkmp.components.OverflowSidePanel
-import uz.otamurod.blogkmp.components.SidePanel
-import uz.otamurod.blogkmp.util.Constants.PAGE_WIDTH
+import org.jetbrains.compose.web.dom.Text
+import uz.otamurod.blogkmp.components.AdminPageLayout
 import uz.otamurod.blogkmp.util.isUserLoggedIn
 
 @Page
@@ -26,28 +18,12 @@ fun HomePage() {
 
 @Composable
 fun HomeScreen() {
-    var isOverflowSidePanelOpened by remember { mutableStateOf(false) }
-
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .maxWidth(PAGE_WIDTH.px )
+    AdminPageLayout {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
         ) {
-            SidePanel(onMenuClick = {
-                isOverflowSidePanelOpened = true
-            })
-
-            if (isOverflowSidePanelOpened){
-                OverflowSidePanel(
-                    onMenuClose = {
-                        isOverflowSidePanelOpened = false
-                    },
-                    content = {})
-            }
+            Text(value = "Home")
         }
     }
 }
